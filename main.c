faf570745c70db04c9213ac59d571ef0c17b15a1
lukasalexandersson98livese <lukas.alexandersson98@live.se>
2025-05-31 16:17:05 +0200

    test1

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+float fahrenheit_to_celsius(float a){
+    float b = 32.0;
+    float c = 5.0;
+    float d = 9.0;
+    float celsius = (a - b) * c / d;
+
+    return celsius;
+}
+
 
 // This is where you add the two functions
 
@@ -8,7 +17,7 @@ int main() {
     float temp_c = 37.0;
 
     printf("%.1f\n", fahrenheit_to_celsius(temp_f));  // Expected output: 37.8
-    printf("%.1f\n", celsius_to_fahrenheit(temp_c));  // Expected output: 98.6
+    //printf("%.1f\n", celsius_to_fahrenheit(temp_c));  // Expected output: 98.6
 
     return 0;
 }
